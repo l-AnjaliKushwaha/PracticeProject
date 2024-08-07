fcf721523d1af8cf6f56ed457b1668bfcc76a8b7
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-08-07 06:52:14 +0530

    "Refactor App component, and update imports"

--- practice02/src/App.jsx
+++ practice02/src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Header  from './Component/Header.jsx'
-import '../src/index.css'
+import SearchBar from './Component/SearchBar.jsx'
+import SelectMenu from './Component/SelectMenu.jsx'
+import CountriesList from './Component/CountriesList.jsx';
+import "./App.css";
 
 function App() {
   return (
     <>
-    <Header/>
+      <Header />
+      <main>
+        <div className="search-filter-container">
+          <SearchBar />
+          <SelectMenu />
+        </div>
+        <CountriesList/>
+      </main>
     </>
-  )
+  );
 }
 
 export default App
---
+++ practice02/src/Component/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SearchBar() {
+  return (
+    <div>
+      <div className="search-container">
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <input type="text" placeholder="Search for a country..." />
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar
